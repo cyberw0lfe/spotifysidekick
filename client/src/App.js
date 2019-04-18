b349dc918dece938e0e03d2ee917ed954f6ab246
A Wolfe <adamtwolfe@gmail.com>
2019-04-17 21:24:35 -0400

    remove node modules (and start of playground)

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ const Profile = lazy(() => import('./components/profile/Profile'))
 const Search = lazy(() => import('./components/search/Search'))
 const Discover = lazy(() => import('./components/discover/Discover'))
 const GenrePlaylist = lazy(() => import('./components/genre-playlist/GenrePlaylist'))
+const Playground = lazy(() => import('./components/common/Playground'))
 
 export default () => (
   <div>
@@ -22,6 +23,7 @@ export default () => (
           <Route path='/search' component={Search} />
           <Route path='/discover' component={Discover} />
           <Route path='/genre-playlist' component={GenrePlaylist} />
+          <Route path='/playground' component={Playground} />
         </Suspense>
       </div>
     </Router>
---
+++ client/src/components/common/Button.jsx
@@ -0,0 +1,9 @@
+const React = require('react')
+
+const style = {
+  
+}
+
+export default (props) => (
+  <button style={style}>{props.text}</button>
+)
